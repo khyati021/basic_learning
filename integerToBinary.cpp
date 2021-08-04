ce35e1868ddb5ea2cc42d92421a35669073fb657
khyati021 <70808001+khyati021@users.noreply.github.com>
2021-08-04 18:46:28 +0530

    Create integerToBinary.cpp
    program to convert integer to binary

---
+++ integerToBinary.cpp
@@ -0,0 +1,28 @@
+// C++ program to convert a decimal
+// number to binary number
+ 
+#include <iostream>
+using namespace std;
+ 
+// function to convert decimal to binary
+int binaryNum[16] ;
+void decToBinary(int n)
+{
+    int i = 0;
+    while (n != 0)
+    {
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+    for (int j = 16; j >= 0 ; j--)
+        cout << binaryNum[j];
+}
+ 
+// Driver program to test above function
+int main()
+{
+    int n = 0x7F; // stroing hexadecimal value
+    decToBinary(n);
+    return 0;
+}
